266262b17b1a2d4c177fc7b9de5bccb93a6ceaf4
Lin <z3632820@gmail.com>
2026-01-07 10:03:47 +0800

    style(DbService): 移除 SqlRepository 中未使用的 using 語句
    清理程式碼，移除 SqlRepository.cs 檔案中多餘且未被使用的 using 指示詞，以改善程式碼的整潔度。

--- DbService/Services/SqlRepository.cs
+++ DbService/Services/SqlRepository.cs
@@ -1,12 +1,6 @@
 ﻿using Dapper;
 using DbService.Interface;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DbService.Services;
 
 
